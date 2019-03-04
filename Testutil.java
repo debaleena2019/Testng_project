fdda29ec7dd018dadc56179639d8a5f6edf91437
debaleena2019 <48118716+debaleena2019@users.noreply.github.com>
2019-03-05 00:12:49 +0530

    Add files via upload

--- Testutil.java
+++ Testutil.java
@@ -1,8 +1,11 @@
 package Com.crm.qa.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Testutil extends Testbase {
+	static String Testdata_path="C:\\JAVA\\Freecrmtestautomation\\src\\main\\java\\Com\\crm\\qa\\testdata\\freecrmtestdata.xlsx";
 public Testutil() throws IOException {
 		super();
 			}
@@ -10,4 +13,16 @@ public Testutil() throws IOException {
 public void switchtoframe() {
 	driver.switchTo().frame("mainpanel");
 	}
+public static Object[][] gettestdata(String sheetname) throws FileNotFoundException{
+	FileInputStream file=null;
+	file=new FileInputStream(Testdata_path);
+	Object sheet = book.getsheet(sheetname);
+	Object[][] data=new Object[sheet.getLastrow()][sheet.getrow().getlastcellnum()];
+	for (int i=0;i<sheet.getLastRowNum();i++) {
+		for(int k=0;k<sheet.getRow(0).getlastCellNum;k++) {
+			data[i][k]=sheet.getow(i+1).getCell(k).toString();
+		}
+	}
+		
+}
 }
