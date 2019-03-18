86be6660f8ca0430c1bf0621135e6ed6b91195da
debaleena2019 <48118716+debaleena2019@users.noreply.github.com>
2019-03-18 08:09:03 +0530

    Add files via upload

--- Homepage.java
+++ Homepage.java
@@ -3,6 +3,7 @@ package Com.crm.qa.pages;
 import java.io.IOException;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -14,14 +15,18 @@ public class Homepage extends Testbase {
 	Dealspage dealspage;
 	Taskpage taskpage;
 
-@FindBy(xpath="//a[contains(text(),'Contacts')]")
-WebElement contacts;
-
-@FindBy(xpath="//td[contains(text(),'User: Debaleena Paul')]")
+	
+//	@FindBy(xpath= "//td[contains(text(),'User: Debaleena Paul')]")
+//WebElement username;
+@FindBy(xpath="//td[@class='headertext']")
 WebElement username;
 
-@FindBy(xpath="//td[contains(text(),'Deals')]")
+@FindBy(xpath= "//a[contains(text(),'Contacts')]")
+WebElement Contacts;
+@FindBy(xpath= "//a[contains(text(),'Deals')]")
 WebElement Deals;
+@FindBy(xpath= "//a[contains(text(),'Tasks')]")
+WebElement Tasks;
 
  public Homepage() throws IOException{
 	 PageFactory.initElements(driver, this);
@@ -31,12 +36,13 @@ WebElement Deals;
  }
  public boolean correctusername() {
 	boolean bol= username.isDisplayed();
+	System.out.println("bol"+bol);
 	return bol;
  }
 
  
  public Contactpage contactsclick() throws IOException{
-	 contacts.click();
+	 Contacts.click();
 	 return new Contactpage(); 
 	 }
  public Dealspage Dealsclick(){
@@ -47,7 +53,11 @@ WebElement Deals;
 	 Deals.click();
 	 return new Taskpage(); 
 	 }
- 
+ public void clickoncontactslink() {
+	 Actions action=new Actions(driver);
+	 action.moveToElement(Contacts).build().perform();
+	Contacts.click();
+ }
 
 }
 
