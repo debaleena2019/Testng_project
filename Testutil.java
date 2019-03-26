f5fa3e565e876fdcf44b4a3ffe650e0c9b2814aa
debaleena2019 <48118716+debaleena2019@users.noreply.github.com>
2019-03-27 00:29:58 +0530

    Add files via upload

--- Testutil.java
+++ Testutil.java
@@ -2,10 +2,19 @@ package Com.crm.qa.base;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class Testutil extends Testbase {
-	static String Testdata_path="C:\\JAVA\\Freecrmtestautomation\\src\\main\\java\\Com\\crm\\qa\\testdata\\freecrmtestdata.xlsx";
+public static String Testdata_path="C:\\JAVA\\Freecrmtestautomation\\src\\main\\java\\Com\\crm\\qa\\testdata\\freecrmtestdata.xlsx";
+static Workbook book;
+static Sheet sheet;	
 public Testutil() throws IOException {
 		super();
 			}
@@ -14,15 +23,30 @@ public void switchtoframe() {
 	driver.switchTo().frame("mainpanel");
 	}
 public static Object[][] gettestdata(String sheetname) throws FileNotFoundException{
-	FileInputStream file=null;
-	file=new FileInputStream(Testdata_path);
-	Object sheet = book.getsheet(sheetname);
-	Object[][] data=new Object[sheet.getLastrow()][sheet.getrow().getlastcellnum()];
-	for (int i=0;i<sheet.getLastRowNum();i++) {
-		for(int k=0;k<sheet.getRow(0).getlastCellNum;k++) {
-			data[i][k]=sheet.getow(i+1).getCell(k).toString();
-		}
+FileInputStream file=null;
+try {
+file=new FileInputStream(Testdata_path);
+}
+catch (FileNotFoundException e)
+{
+	e.printStackTrace();
+}
+try {
+	book=WorkbookFactory.create(file);
+}catch(InvalidFormatException e) {
+	e.printStackTrace();
+}
+catch(IOException e) {
+	e.printStackTrace();
+	
+}
+  Sheet sheet = book.getSheet(sheetname);
+Object[][] data=new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+for (int i=0;i< sheet.getLastRowNum();i++) {
+for(int k=0;k<sheet.getRow(0).getLastCellNum();k++) {
+data[i][k]=sheet.getRow(i+1).getCell(k).toString();
 	}
-		
+	}
+	return data	;
 }
 }
